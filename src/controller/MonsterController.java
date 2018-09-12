af8c0d4260d7cde986fe29844f9c6338318659aa
Popcornblaster <lucinablaster@gmail.com>
2018-09-12 13:38:55 -0600

    Added different variables to be able to set and get variables from the system. Also added comments about other additions.

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -1,23 +1,59 @@
 package controller;
 
 import model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
 	// Data member section
 	//Declaration section
 	private MarshmallowMonster myMonster;
+	private MarshmallowMonster userMonster;
+	private Scanner inputScanner;
 	
 	//Constructors initialize data members@
 	public MonsterController()
 	{
 		myMonster = new MarshmallowMonster("Harold", 2.0, 3, true, 4, true);
-		
+		userMonster = new MarshmallowMonster();
+		inputScanner = new Scanner(System.in);
 	}
 	
 	public void start()
 	{
-		System.out.println(myMonster);
+		System.out.println("We will be setting the params for a monster, what will you name it?");
+		String name = inputScanner.next();
+		userMonster.setName(name);
+		
+		System.out.println("How many legs would you like your monster to have?");
+		double legCount = inputScanner.nextInt();
+		userMonster.setLegCount(legCount);
+				
+		System.out.println("How many eyes does your monster have?");
+		int eyeCount = inputScanner.nextInt();
+		userMonster.setEyeCount(eyeCount);
+		
+
+		System.out.println("Does your monster have a nose?");
+		boolean hasNoses = inputScanner.nextBoolean();
+		userMonster.setHasNoses(hasNoses);
+		
+		System.out.println("How many arms would you like for your monster?");
+		int arms = inputScanner.nextInt();
+		userMonster.setArmCount(arms);
+		
+		System.out.println("Does your monster have ears?");
+		boolean hasEars = inputScanner.nextBoolean();
+		userMonster.setHasEars(hasEars);
+		
+		//System.out.println("Your monster has " + userMonster.getArmCount() + " arms!");
+		
+		
+		
+		//System.out.println("We will be making a monster, what would you like to name it?");
+		//Scanner inputScanner = new Scanner(System.in);
+		System.out.println(userMonster);
+		inputScanner.close();
 	}
 
 }
--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -2,25 +2,25 @@ package model;
 
 public class MarshmallowMonster
 {
-	//-----------Declaration Section------//
+	// -----------Declaration Section------//
 	private String name;
 	private double legCount;
 	private int eyeCount;
 	private boolean hasNoses;
 	private int armCount;
 	private boolean hasEars;
-	
+
 	public MarshmallowMonster()
 	{
-	//Default constructor
-	//Generally boring and not as helpful
+		// Default constructor
+		// Generally boring and not as helpful
 	}
-	
+
 	public MarshmallowMonster(String name)
 	{
-		this.name = name ; 
+		this.name = name;
 	}
-	
+
 	public MarshmallowMonster(String name, double legCount, int eyeCount, boolean hasNose, int arms, boolean ears)
 	{
 		this.name = name;
@@ -30,17 +30,77 @@ public class MarshmallowMonster
 		this.armCount = arms;
 		this.hasEars = ears;
 	}
+
+	public String getName()
+	{
+		return name;
+	}
+	
+	public double getLegCount()
+	{
+		return legCount;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public boolean getHasNoses()
+	{
+		return hasNoses;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setLegCount(double legCount)
+	{
+		this.legCount = legCount;
+	}
+	
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	
+	public void setHasNoses(boolean hasNoses)
+	{
+		this.hasNoses = hasNoses;
+	}
+	
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	
+	public void setHasEars(boolean hasEars)
+	{
+		this.hasEars = hasEars; 
+	}
+	
+	public boolean getHasEars()
+	{
+		return hasEars;
+	}
+	
+	
+	
 	
 	public String toString()
 	{
-		String description = "This monster is named " + name
-				+ " and it has " + legCount +" legs,"
-				+ " and " + eyeCount +" eyes,"
-				+ " and it's " + hasNoses +" that it has a nose,"
-				+ " and " + armCount +" arms,"
-				+ " and it's " + hasEars +" that is has ears."; 
-		
-		return description; 
+		String description = "This monster is named " + name + " and it has " + legCount + " legs," + " and " + eyeCount
+				+ " eyes," + " and it's " + hasNoses + " that it has a nose," + " and " + armCount + " arms,"
+				+ " and it's " + hasEars + " that is has ears.";
+
+		return description;
 	}
-	
+
 }
